1884679e9b84d6acc67cdf8e6451845fa3986da4
minh <hadtnt71@gmail.com>
2025-12-25 17:03:26 +0700

    25/12

--- leetcode/22-12/ValidPalindrome.java
+++ leetcode/22-12/ValidPalindrome.java
@@ -6,13 +6,28 @@ public class ValidPalindrome {
     int right = s.length() - 1;
 
     while (left < right) {
+      while (left < right && !Character.isLetterOrDigit(s.charAt(left))) {
+        left++;
+      }
 
+      while (left < right && !Character.isLetterOrDigit(s.charAt(right))) {
+        right--;
+      }
+
+      if (Character.toLowerCase(s.charAt(left)) != Character.toLowerCase(s.charAt(right))) {
+        return false;
+      }
+
+      left++;
+      right--;
     }
+
+    return true;
   }
 
   public static void main(String[] args) {
     ValidPalindrome solution = new ValidPalindrome();
-    String s = "racecar";
+    String s = "Was it a car or a cat I saw?";
     boolean result = solution.isPalindrome(s);
     System.out.println("Is palindrome: " + result);
   }
